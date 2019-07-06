14ab79e4e23bf876b4409acff892e7b2f71ed0a2
Mateus Vinicius da Silva <36384659+MateusVS@users.noreply.github.com>
2019-07-06 18:08:18 -0300

    Versão Final

--- calc.js
+++ calc.js
@@ -1,67 +1,67 @@
-function calcula (valor){
-	var x=document.calc.tela.value;
+function calcula(valor) {
+    var x = document.calc.tela.value;
 
-	document.calc.tela.value=x+valor;
-
-}
-function resul (){
-
-	var conta = document.getElementById('tela').value;
-	if (conta==='') {
-		alert("Impossivel calcular, os valores não foram informados");
-	}
-	else{
-		var resultado = (eval(conta));
-		document.getElementById('tela').value = resultado;
-
-		document.getElementsByClassName('botao')
-	}
+    document.calc.tela.value = x + valor;
 
 }
-function raiz(){
-	var i=parseInt(document.calc.tela.value);
-	var tratamento = (isNaN(i));
-	if (tratamento === true){
-		alert("Impossivel calcular, os valores não foram informados");		
-	}
-	else{	
-		var y = Math.sqrt(i);
-		document.calc.tela.value=y;
-	}
+
+function resul() {
+
+    var conta = document.getElementById('tela').value;
+    if (conta === '') {
+        alert("Impossivel calcular, os valores não foram informados");
+    } else {
+        var resultado = (eval(conta));
+        document.getElementById('tela').value = resultado;
+
+        document.getElementsByClassName('botao')
+    }
+
 }
-function quadrado(){
-	var x=parseInt(document.calc.tela.value);
-	
-	var teste = (isNaN(x));
-	if (teste === true){
-		alert("Impossivel calcular, os valores não foram informados");		
-	}
-	else{		
-		var resul = Math.pow(x,2);
-		document.calc.tela.value=resul;
-	}
+
+function raiz() {
+    var i = parseInt(document.calc.tela.value);
+    var tratamento = (isNaN(i));
+    if (tratamento === true) {
+        alert("Impossivel calcular, os valores não foram informados");
+    } else {
+        var y = Math.sqrt(i);
+        document.calc.tela.value = y;
+    }
 }
-function cubo(){
-	var z=parseInt(document.calc.tela.value);
-	
-	var teste = (isNaN(z));
-	if (teste === true){
-		alert("Impossivel calcular, os valores não foram informados");		
-	}
-	else{		
-		var final = Math.pow(z,3);
-		document.calc.tela.value=final;
-	}
+
+function quadrado() {
+    var x = parseInt(document.calc.tela.value);
+
+    var teste = (isNaN(x));
+    if (teste === true) {
+        alert("Impossivel calcular, os valores não foram informados");
+    } else {
+        var resul = Math.pow(x, 2);
+        document.calc.tela.value = resul;
+    }
 }
-function log(){
-	var a=parseInt(document.calc.tela.value);
-	var teste = (isNaN(a));
-	if (teste === true){
-		alert("Impossivel calcular, os valores não foram informados");		
-	}
-	else{		
-		b = Math.log(a);
-		document.calc.tela.value=b;
-	}
+
+function cubo() {
+    var z = parseInt(document.calc.tela.value);
+
+    var teste = (isNaN(z));
+    if (teste === true) {
+        alert("Impossivel calcular, os valores não foram informados");
+    } else {
+        var final = Math.pow(z, 3);
+        document.calc.tela.value = final;
+    }
+}
+
+function log() {
+    var a = parseInt(document.calc.tela.value);
+    var teste = (isNaN(a));
+    if (teste === true) {
+        alert("Impossivel calcular, os valores não foram informados");
+    } else {
+        b = Math.log(a);
+        document.calc.tela.value = b;
+    }
 
 }
